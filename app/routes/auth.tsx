70e88b6883c139029ade7e238b835bbf64b52406
bk-kiran <kiranbk1704@gmail.com>
2025-07-22 23:42:08 +0800

    commit

---
+++ app/routes/auth.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+export const meta = () => ([
+    {title: 'AI Resume Analyzer | Auth'},
+    {name: 'description', content: "Login to Account"}
+])
+
+const auth = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default auth
