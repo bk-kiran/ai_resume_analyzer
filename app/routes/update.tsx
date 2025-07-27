98136cc18a15d602ce559479442a7f724178393b
bk-kiran <kiranbk1704@gmail.com>
2025-07-27 14:28:59 +0800

    commit

--- app/routes/update.tsx
+++ app/routes/update.tsx
@@ -1,33 +1,60 @@
 import React, { useState, type FormEvent } from 'react'
 import Navbar from '~/components/Navbar'
 import FileUploader from '~/components/FileUploader'
+import { usePuterStore } from '~/lib/puter'
+import { useNavigate } from 'react-router'
 
 const update = () => {
-const [isProcessing, setIsProcessing] = useState(false); {/* useState to check if upload is processing*/}
-const [statusText, setStatusText] = useState('')
-const [setFile, setFileState] = useState<File | null>(null); {/* Uploaded file is passed from file uploader to update.tsx and saved in this state*/}
+    const {auth, isLoading, fs, ai, kv} = usePuterStore(); {/* puter.js functions: auth, loading, file storage, AI, key/value storage */}
+    const navigate = useNavigate();
+    const [isProcessing, setIsProcessing] = useState(false); {/* useState to check if upload is processing*/}
+    const [statusText, setStatusText] = useState('')
+    const [file, setFileState] = useState<File | null>(null); {/* Uploaded file is passed from file uploader to update.tsx and saved in this state*/}
 
-{/* Function to help with file uploads */}
-const handleFileSelect = (file: File | null) => {
-    setFileState(file)
-} 
+    {/* Function to help with file uploads */}
+    const handleFileSelect = (file: File | null) => {
+        setFileState(file)
+    } 
+
+    {/* Function to prepare Resume for Analyzing Process */}
+    const handleAnalyze = async({companyName, jobTitle, jobDescription, file}: {companyName: string, jobTitle: string, jobDescription: string, file: File}) => {
+        setIsProcessing(true); {/* Start processing */}
+        setStatusText('Uploading Your Resume...')
+
+        const uploadedFile = await fs.upload([file]); // uplaoding resume file to Puter Cloud Storage
+        if (!uploadedFile) return setStatusText('Error: Failed to Upload Your Resume :/')
+        
+        setStatusText("Upload Sucess: Converting Resume from PDF to Image...")
+        //const imageFile  = await convertPDFToImage(file);
 
 
-{/* */}
-const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault(); // prevent reloading of browser
-    const form = e.currentTarget.closest('form'); // gathering form data
-    if(!form) return; 
-    const formData = new FormData(form) // storing data in formData
 
-    const companyName = formData.get('company-name')
-    const jobTitle = formData.get('job-title')
-    const jobDescription = formData.get('job-description')
 
-{/*    console.log({
-        companyName,jobTitle, jobDescription, setFile
-    })*/}
-}
+    }
+
+
+
+    {/* Once Form is SUbmitted */}
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault(); // prevent reloading of browser
+        const form = e.currentTarget.closest('form'); // gathering form data
+        if(!form) return; 
+        const formData = new FormData(form) // storing data in formData
+
+        const companyName = formData.get('company-name') as string
+        const jobTitle = formData.get('job-title') as string
+        const jobDescription = formData.get('job-description') as string
+
+        // {   console.log({
+        //     companyName,jobTitle, jobDescription, file
+        // })}
+
+        // if no file when button is submitted then return nothing
+        if(!file) return;
+
+        // else call handleAnalyze to start resume analysis
+        handleAnalyze({companyName, jobTitle, jobDescription, file})
+    }
 
   return (
         <main className="min-h-screen bg-[url('/images/bg-main.svg')] bg-cover bg-center bg-no-repeat bg-fixed">
